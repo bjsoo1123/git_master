006ab14d7e58fb651747b497b6e1d4a1ebd1e941
bjsoo1123 <bjsoo1123@naver.com>
2019-05-01 11:04:49 +0000

    setting

--- gitmaster/gitmaster/settings.py
+++ gitmaster/gitmaster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b^oee+6a^%s7+0w*l=pr5%p3-6btw0tcbs57!k)w=d70wyn3he'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'gitmaster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'gitmaster', 'static')
+    ]
